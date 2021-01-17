c46c408119120e758879b41e1cafc773570e0b05
Jose <joserdz052@outlook.com>
2021-01-16 20:59:31 -0600

    Creo decimal a hexadecimal

---
+++ decimalHexadecimal.c
@@ -0,0 +1,12 @@
+    #include <stdio.h>
+    #include <stdlib.h>
+     
+    int main()
+    {
+        int decimal;
+     
+        printf("introduzca un numero decimal: ");
+        scanf("%d", &decimal);
+        printf("el numero en hexadecimal es: %x\n", decimal);
+        system("pause");
+    }
